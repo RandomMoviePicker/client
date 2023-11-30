67d7e0da1ebc43827f8038f20fdb4f81b2256b64
Marc.A <marcalberopascual@gmail.com>
2023-11-30 17:16:01 +0100

    create playlists

---
+++ src/pages/PlaylistsForm/playlistsForm.jsx
@@ -0,0 +1,45 @@
+import "./playlistsForm.css";
+import {useState, useContext} from "react";
+import { AuthContext } from "../../context/auth.context";
+const URL = import.meta.env.VITE_SERVER_URL;
+
+
+const PlaylistsForm = () => {
+  const { user } = useContext(AuthContext);
+  const userId = user._id;
+  const [name , setName] = useState("");
+
+const handleInputChange = (event) =>{
+  const currentValue = event.target.value;
+  setName(currentValue);
+}
+const handleFormSubmit = async(event) =>{
+  event.preventDefault();
+  try{
+    await fetch(URL + "/movies/createPlaylist", {
+      method: "POST",
+      headers: {
+        "Content-Type":"application/json",
+      },
+      body: JSON.stringify({name, userId})
+
+    })
+  }
+  catch(error){
+    console.log(error)
+  }
+}
+
+
+  return (
+    <form onSubmit={(event)=>handleFormSubmit(event)} className="playlistForm">
+        
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" name="name" value={name} onChange={(event) => handleInputChange(event)} />
+
+        <button type="submit">Create playlist</button>
+      </form>
+  );
+}
+
+export default PlaylistsForm;

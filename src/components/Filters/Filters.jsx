a8d7c4e3a681ad7d43a74dae5731654b7e06325f
Alba Garcia Gonzalez <garciagonzalezalba@gmail.com>
2023-11-30 13:24:42 +0100

    filters working

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -1,33 +1,48 @@
 import genres from '../../genres.json'
 import { useState } from 'react'
+const URL = import.meta.env.VITE_SERVER_URL
 
 const Filters = () =>{
-    const [genresArr, setGenresArr] = useState(genres);
+    const [includesGenresArr, setIncludesGenresArr] = useState(genres);
+    const [excludesGenresArr, setExcludesGenresArr] = useState([]);
 
     const handleChange= (event) =>{
-        // const value = event
         const value = event.target.value
-        
-
-        console.log("check", value);
+        const copyGenresArr = [...includesGenresArr]
+        const updatedGenreArr = copyGenresArr.filter((eachGenre)=>{
+            if(eachGenre !== value)
+            {
+                return eachGenre;
+            }
+            else{
+                setExcludesGenresArr((previous)=>[...previous, eachGenre])
+            }
+        })
+        setIncludesGenresArr(updatedGenreArr)
     }
+    const fetchFiltered = async () =>{
+        const response = await fetch(`${URL}/movies/filter?included=${includesGenresArr}&excluded=${excludesGenresArr}`)
+        const responsejson = await response.json();
+        console.log(responsejson)
+    }
+
     return(
         <>
             <h1>filters</h1>
             {
-                genresArr.map((eachGenre)=>{
+                includesGenresArr.map((eachGenre)=>{
                     return(
-                        <div key={eachGenre.id} >
+                        <div key={eachGenre} >
                             <label >
-                                <input value={eachGenre.name} type="checkbox" onChange={(event)=>handleChange(event)} />
-                                {eachGenre.name}
+                                <input value={eachGenre} type="checkbox" onChange={(event)=>handleChange(event)} />
+                                {eachGenre}
                             </label>
                         </div>
                     )
                 })
             }
+            <button onClick={()=>fetchFiltered()}>Filter</button>
         </>
-        
     )
 }
 
